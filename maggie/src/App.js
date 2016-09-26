fc7c196d1f770e96162c5464fb32c819c767ad5c
Maggie Merrell <maggiemerrell31@gmail.com>
2016-09-26 08:11:39 -0400

    switched to react

---
+++ maggie/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container-fluid">
+          <ul className="header-bar">
+            <li>About</li>
+            <li>Coding Projects</li>
+            <li>Art Projects</li>
+            <li>Resume</li>
+            <li className="last">Contact</li>
+          </ul>
+        </div>
+        <h3 className="floating-name">MAGGIE MERRELL</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
